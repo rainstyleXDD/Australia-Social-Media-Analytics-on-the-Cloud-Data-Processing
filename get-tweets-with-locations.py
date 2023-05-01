66691013f645d9e0920352fb2d2cf27fe6ffeab5
rainstyleXDD <longhc589@gmail.com>
2023-05-01 21:34:36 +1000

    modified get tweets with location to smaller pickled json

--- get-tweets-with-locations.py
+++ get-tweets-with-locations.py
@@ -10,13 +10,15 @@ def main():
     size = comm.Get_size()
     rank = comm.Get_rank()
 
+    pickle_size = 100000
+
     t1_start = time.perf_counter()
     path = "./twitter-huge.json/mnt/ext100/twitter-huge.json"
     # with open('sal.json', "rb") as f:
     #     sal = json.load(f)
 
-    with open(path, "r") as f, open('tweetsWithLocation' + str(rank) + '.json', 'w') as f_output:
-        f_output.write("[\n")
+    with open(path, "r") as f:
+        
 
         # get size of the file and partition the file
         tweet_file_size = os.path.getsize(path)
@@ -26,6 +28,8 @@ def main():
 
         #print(rank, start, end)
         count = 0
+        total_count = 0
+        pickle_count = 0
         # find end _id
         f.seek(end, 0)
         
@@ -38,9 +42,9 @@ def main():
         #print(rank, end_id)
 
         f.seek(start, 0)        # Moves read/write head to the correct position
-
+        f_output = open('tweetsWithLocation' + str(rank)+ '_' +str(pickle_count) + '.json', 'w')
+        f_output.write("[\n")
         for line in f:
-            count += 1
             
             if (line.startswith('{"id":')):
                 # check end partition
@@ -50,25 +54,49 @@ def main():
                 
                 # store json with locations
                 if '"full_name"' in line:
+                    count += 1
                     f_output.write(line)
+
+                    # close and open write new file when tweets read exceeds pickle size
+                    if count >= pickle_size:
+                        f_output.write(']\n')
+                        f_output.close()
+                        
+                        pickle_count += 1
+                        total_count += count
+                        count = 0
+                        f_output = open('tweetsWithLocation' + str(rank)+ '_' +str(pickle_count) + '.json', 'w')
+                        f_output.write("[\n")
+        total_count += count
+        f_output.write(']\n')
+        f_output.close()
+                    
+
+                
+
+    # clean the ',' at the end of file for valid json  
+    for i in range(pickle_count+1):
+        with open('tweetsWithLocation' + str(rank)+ '_' +str(i) + '.json', 'r') as file:
+        # read a list of lines into data
+            data = file.readlines()
+            
+            if data[-2][-2] == ',':
+                data[-2] = data[-2][:-2] + '\n'
+
+        # and write everything back
+        with open('tweetsWithLocation' + str(rank)+ '_' +str(i) + '.json', 'w') as file:
+            file.writelines(data)
+
             
 
-        f_output.write(']\n')
-        print(rank, count)
+
+        print(rank, total_count)
+        t1_end = time.perf_counter()
+        print(rank, t1_end - t1_start)
        
     
 
-    with open('tweetsWithLocation' + str(rank) + '.json', 'r') as file:
-    # read a list of lines into data
-        data = file.readlines()
-
-        # now change the 2nd line, note that you have to add a newline
-        if data[-2][-2] == ',':
-            data[-2] = data[-2][:-2] + '\n'
-
-    # and write everything back
-    with open('tweetsWithLocation' + str(rank) + '.json', 'w') as file:
-        file.writelines(data)
+    
     
     return 
             
