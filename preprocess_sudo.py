6d5756d16189b7af77d6cc1ac6f6a3b3f2b2eec1
Jiayao Lu <jiayaol2@student.unimelb.edu.au>
2023-05-18 23:33:55 +1000

    upload new curated data and lga data

--- preprocess_sudo.py
+++ preprocess_sudo.py
@@ -6,6 +6,7 @@ import couchdb
 # input and output file path
 file_path = './rai_business_indicators_lga_2011-5781914874165984729.json'
 out_file_path = './business_indicator.json'
+out_file_path_2 = './lga.json'
 
 # extract the useful data from sudo
 with open(file_path, 'r') as file, open(out_file_path, 'w') as out_file:
@@ -21,8 +22,23 @@ with open(file_path, 'r') as file, open(out_file_path, 'w') as out_file:
     out_file.write(']')
 
 # save the data in couchDB
-couch = couchdb.Server('http://admin:password@172.26.131.66:5984/')
+couch = couchdb.Server('http://admin:password@172.26.133.215:5984/')
 db = couch['sudo']
 with open('./business_indicator.json', 'r') as f:
     sudo_data = json.load(f)
     db.update(sudo_data)
+
+# Opening sudo JSON file
+with open(out_file_path, 'r') as lga, open(out_file_path_2, 'w') as output:
+
+    # returns JSON object as a dictionary
+    lga_data = json.load(lga)
+    lga_dict = dict()
+
+    for i in range(len(lga_data)):
+        lganame = lga_data[i]['lganame'][:-4].lower()
+        lga_dict[lganame] = {'fullname': lga_data[i]['lganame'],
+                            'state': lga_data[i]['state']}
+
+    json_object_2 = json.dumps(lga_dict, indent=2)
+    output.write(json_object_2)
